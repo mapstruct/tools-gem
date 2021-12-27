bd890b3911cd9e751b347971252c4e396ee52dc5
Filip Hrisafov <filip.hrisafov@gmail.com>
2021-12-27 08:28:38 +0100

    Add tests for verifying the contents of the generated gem with the expected content

--- processor/src/test/java/org/mapstruct/tools/gem/processor/ProcessorTest.java
+++ processor/src/test/java/org/mapstruct/tools/gem/processor/ProcessorTest.java
@@ -5,9 +5,15 @@
  */
 package org.mapstruct.tools.gem.processor;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URI;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
 import javax.annotation.processing.Processor;
 import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
@@ -18,6 +24,7 @@ import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 
+import org.assertj.core.api.JUnitSoftAssertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -29,6 +36,9 @@ public class ProcessorTest {
     // CHECKSTYLE:OFF
     @Rule
     public final TemporaryFolder tempDir = new TemporaryFolder(); //NOSONAR
+
+    @Rule
+    public final JUnitSoftAssertions softly = new JUnitSoftAssertions();
     // CHECKSTYLE:ON
 
     @Test
@@ -45,7 +55,10 @@ public class ProcessorTest {
 
         DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
         StandardJavaFileManager fileManager = compiler.getStandardFileManager( diagnostics, null, null );
-        fileManager.setLocation( StandardLocation.CLASS_OUTPUT, Arrays.asList( tempDir.getRoot() ) );
+        File classesDir = tempDir.newFolder( "classes" );
+        fileManager.setLocation( StandardLocation.CLASS_OUTPUT, Collections.singletonList( classesDir ) );
+        File generatedDir = tempDir.newFolder( "generated" );
+        fileManager.setLocation( StandardLocation.SOURCE_OUTPUT, Collections.singletonList( generatedDir ) );
 
         JavaCompiler.CompilationTask task = compiler.getTask(
             null,
@@ -65,6 +78,32 @@ public class ProcessorTest {
             System.err.println( diagnostic );
         }
         assertThat( success ).isTrue();
+
+
+        assertGeneratedFileContent( "BuilderGem", generatedDir );
+        assertGeneratedFileContent( "SomeAnnotationGem", generatedDir );
+        assertGeneratedFileContent( "SomeAnnotationsGem", generatedDir );
+        assertGeneratedFileContent( "SomeArrayAnnotationGem", generatedDir );
+    }
+
+    protected void assertGeneratedFileContent(String gemName, File generatedDir) {
+        Path gemPath = generatedDir.toPath().resolve( "org/mapstruct/tools/gem/processor/" + gemName + ".java" );
+        softly.assertThat( gemPath )
+            .as( gemName )
+            .exists();
+
+        try (InputStream generatedGemStream = new FileInputStream( gemPath.toFile() );
+             InputStream expectedGemStream = getClass().getClassLoader()
+                 .getResourceAsStream( "fixtures/org/mapstruct/tools/gem/processor/" + gemName + ".java" )
+        ) {
+            softly.assertThat( generatedGemStream )
+                .as( gemName )
+                .hasSameContentAs( expectedGemStream );
+        }
+        catch ( IOException e ) {
+            throw new UncheckedIOException( "Failed to assert generated content for gem " + gemName, e );
+        }
+
     }
 
     private static class StringJavaFileObject extends SimpleJavaFileObject {
